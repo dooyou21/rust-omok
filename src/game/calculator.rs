0013690e8c809d8bb1175a675b86553fd9d98a70
dooyou21 <cubeclock94@gmail.com>
2021-06-08 02:19:09 +0900

    ENH: board가 비었는지 판단하는 함수 구현

--- src/game/calculator.rs
+++ src/game/calculator.rs
@@ -9,32 +9,46 @@ enum Direction {
 }
 
 pub fn find_next_point(color: &Color, board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> Point {
-  let mut x = rand::thread_rng().gen_range(0..19);
-  let mut y = rand::thread_rng().gen_range(0..19);
+  let mut x: usize;
+  let mut y: usize;
 
-  // 1. 동일 방향으로 인접한 돌멩이가 4개인 것이 있는가?
-  // 1-1. 나와 동일한 색인가? 4개와 인접한 곳에 둔다.
+  if is_empty(board) {
+    // 보드가 비어있는 경우9,9(가운데)에 둔다.
+    x = 9;
+    y = 9;
+  } else {
+    // 1. 동일 방향으로 인접한 돌멩이가 4개인 것이 있는가?
+    // 1-1. 나와 동일한 색인가? 4개와 인접한 곳에 둔다.
 
-  // 2. 동일 방향으로 인접한 돌멩이가 3개인 것이 있는가? 있으면 양 끝의 점이 후보가 된다.
-  // 좌/상 or 우/하 중에 어떻게 선택할 것인가?
-  // 1) 같은 방향으로 인접한 돌멩이가 있는 곳을 고른다
-  // 2) 주변 8방위 안에 인접한 돌멩이가 있는 쪽에 둔다 (더 많은 쪽에 둔다)
-  // 모든 조건이 동일한 경우 10,10에 가까운 점에 둔다.
+    // 2. 동일 방향으로 인접한 돌멩이가 3개인 것이 있는가? 있으면 양 끝의 점이 후보가 된다.
+    // 좌/상 or 우/하 중에 어떻게 선택할 것인가?
+    // 1) 같은 방향으로 인접한 돌멩이가 있는 곳을 고른다
+    // 2) 주변 8방위 안에 인접한 돌멩이가 있는 쪽에 둔다 (더 많은 쪽에 둔다)
+    // 모든 조건이 동일한 경우 10,10에 가까운 점에 둔다.
 
-  // 3. 동일 방향으로 인접한 돌멩이가 2개인 것이 있는가? 있으면 양 끝점이 후보가 된다.
-  // 주변 8방위에 상대방 색의 돌멩이 하나당 1점으로 계산하여 점수가 낮은 것을 선택한다.
-  // 모든 조건이 동일한 경우 10,10에 가까운 점에 둔다.
+    // 3. 동일 방향으로 인접한 돌멩이가 2개인 것이 있는가? 있으면 양 끝점이 후보가 된다.
+    // 주변 8방위에 상대방 색의 돌멩이 하나당 1점으로 계산하여 점수가 낮은 것을 선택한다.
+    // 모든 조건이 동일한 경우 10,10에 가까운 점에 둔다.
 
-  // 4. 나와 동일한 색의 점을 찾고, 8방위 중에서 랜덤으로 한 자리를 골라서 둔다.
-
-  // 5. 보드가 비어있는 경우9,9(가운데)에 둔다.
-
-  while board[x][y] != PointStatus::Empty {
+    // 4. 나와 동일한 색의 점을 찾고, 8방위 중에서 랜덤으로 한 자리를 골라서 둔다.
     x = rand::thread_rng().gen_range(0..19);
     y = rand::thread_rng().gen_range(0..19);
+
+    while board[x][y] != PointStatus::Empty {
+      x = rand::thread_rng().gen_range(0..19);
+      y = rand::thread_rng().gen_range(0..19);
+    }
   }
 
   Point::new(x, y)
+}
+
+pub fn is_empty(board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> bool {
+  board.into_iter().flatten().all(|&ps| match ps {
+    PointStatus::Empty => true,
+    PointStatus::Black(_) => false,
+    PointStatus::White(_) => false,
+  })
 }
 
 pub fn check_game_end(board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> bool {
