1529e5940c228ab7942d685ede4adbb5f29525d0
dooyou21 <cubeclock94@gmail.com>
2021-06-08 00:58:52 +0900

    ENH: user input을 받을 수 있도록 처리

--- src/game.rs
+++ src/game.rs
@@ -22,6 +22,7 @@ impl Point {
   }
 }
 
+#[derive(Debug)]
 enum Color {
   Black,
   White,
@@ -53,9 +54,38 @@ pub fn run() {
   let mut board = [[PointStatus::Empty; BOARD_SIZE]; BOARD_SIZE];
   let mut turn: Player = Player::Player1;
   let mut is_game_end: bool = false;
-  let mut count = 10;
+
+  print!("\nChoose your stone color - (b)lack / (w)hite : ");
+  io::stdout().flush().unwrap();
+
+  let mut input_line = String::new();
+  let mut player_colors = [Color::Black, Color::White]; // [computer, user];
+
+  loop {
+    io::stdin().read_line(&mut input_line).unwrap();
+    let input = input_line.trim();
+    if input.eq("b") || input.eq("B") {
+      player_colors[1] = Color::Black;
+      player_colors[0] = Color::White;
+      turn = Player::Player2;
+      break;
+    } else if input.eq("w") || input.eq("W") {
+      player_colors[1] = Color::White;
+      player_colors[0] = Color::Black;
+      turn = Player::Player1;
+      break;
+    } else {
+      print!("enter (b) or (w)");
+      io::stdout().flush().unwrap();
+      continue;
+    }
+  }
 
   print_board(&board);
+  println!(
+    "user:{:?}, computer:{:?}",
+    player_colors[1], player_colors[0]
+  );
 
   loop {
     if is_game_end {
@@ -64,28 +94,28 @@ pub fn run() {
 
     match turn {
       Player::Player1 => {
-        let next_point: Point = findPoint(Color::White, &board);
-        place_stone(next_point.x, next_point.y, Color::White, &mut board);
+        // computer
+        let next_point: Point = find_point(&player_colors[0], &board);
+        place_stone(next_point.x, next_point.y, &player_colors[0], &mut board);
+        print_board(&board);
         turn = Player::Player2;
       }
       Player::Player2 => {
-        let next_point: Point = findPoint(Color::Black, &board);
-        place_stone(next_point.x, next_point.y, Color::Black, &mut board);
+        // user
+        let next_point = get_position_from_user();
+        place_stone(next_point.x, next_point.y, &player_colors[1], &mut board);
+        print_board(&board);
         turn = Player::Player1;
-        // TODO: Player2를 유저의 input을 받도록 한다.
       }
     }
-    if count - 1 < 0 {
-      is_game_end = true;
-    } else {
-      count -= 1;
-    }
+
+    is_game_end = check_game_end(&board);
   }
 
   print_board(&board);
 }
 
-fn findPoint(color: Color, board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> Point {
+fn find_point(color: &Color, board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> Point {
   let mut x = rand::thread_rng().gen_range(0..19);
   let mut y = rand::thread_rng().gen_range(0..19);
 
@@ -97,10 +127,15 @@ fn findPoint(color: Color, board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> P
   Point::new(x, y)
 }
 
+fn check_game_end(board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) -> bool {
+  // TODO: 게임이 끝났는지 판단할 수 있어야 함.
+  false
+}
+
 fn place_stone(
   x: usize,
   y: usize,
-  color: Color,
+  color: &Color,
   board: &mut [[PointStatus; BOARD_SIZE]; BOARD_SIZE],
 ) {
   // if (board[x][y] != PointStatus.Empty) 일때 에러처리 필요함.
@@ -111,6 +146,20 @@ fn place_stone(
     Color::Black => PointStatus::Black(point),
   };
   board[x][y] = point_status;
+}
+
+fn get_position_from_user() -> Point {
+  print!("x: ");
+  io::stdout().flush().unwrap();
+  let mut x_input = String::new();
+  io::stdin().read_line(&mut x_input).unwrap();
+  let x = x_input.trim().parse::<usize>().unwrap();
+  print!("y: ");
+  io::stdout().flush().unwrap();
+  let mut y_input = String::new();
+  io::stdin().read_line(&mut y_input).unwrap();
+  let y = y_input.trim().parse::<usize>().unwrap();
+  Point::new(x, y)
 }
 
 fn print_board(board: &[[PointStatus; BOARD_SIZE]; BOARD_SIZE]) {
