e70fc99220c628c394363763211e809ee9ebbebd
dooyou21 <cubeclock94@gmail.com>
2021-06-16 04:55:38 +0900

    ENH: scoreboard 껍데기 구현

--- src/game/calculator.rs
+++ src/game/calculator.rs
@@ -9,17 +9,6 @@ pub fn find_next_point(just_before_point: &Point, color: &Color, board: &Board) 
       // 내가 처음 두는 경우. 보드가 비어있으므로 가운데에 둔다.
       Point::new(9, 9)
     }
-    _ => {
-      let (h_left, h_right) = board.get_directed_points(just_before_point, Direction::Horizontal);
-      let (v_left, v_right) = board.get_directed_points(just_before_point, Direction::Vertical);
-      let (i_left, i_right) = board.get_directed_points(just_before_point, Direction::Increase);
-      let (d_left, d_right) = board.get_directed_points(just_before_point, Direction::Decrease);
-
-      println!("{:?} {:?}", h_left, h_right);
-      println!("{:?} {:?}", v_left, v_right);
-      println!("{:?} {:?}", i_left, i_right);
-      println!("{:?} {:?}", d_left, d_right);
-      find_point_v2::find(color, just_before_point, board)
-    }
+    _ => find_point_v2::find(just_before_point, color, board),
   }
 }
--- src/game/calculator/find_point_v2.rs
+++ src/game/calculator/find_point_v2.rs
@@ -1,9 +1,32 @@
-use crate::game::{Board, Color, Direction, Point, PointStatus};
+use crate::game::{Board, Color, Direction, Point, PointStatus, BOARD_SIZE};
 use rand::prelude::*;
 
-pub fn find(color: &Color, point: &Point, board: &Board) -> Point {
-  let mut next_point: Point;
+struct ScoreBoard {
+  board: [[u16; BOARD_SIZE]; BOARD_SIZE],
+}
 
+impl ScoreBoard {
+  fn new() -> ScoreBoard {
+    ScoreBoard {
+      board: [[0; BOARD_SIZE]; BOARD_SIZE],
+    }
+  }
+  fn set(&mut self, point: &Point, score: u16) {
+    self.board[point.y][point.x] = score;
+  }
+}
+
+pub fn find(just_before_point: &Point, color: &Color, board: &Board) -> Point {
+  let mut score_board = ScoreBoard::new();
+
+  for x in 0..BOARD_SIZE {
+    for y in 0..BOARD_SIZE {
+      let anchor = Point::new(x, y);
+      score_board.set(&anchor, get_point(&anchor, color, board));
+    }
+  }
+
+  let mut next_point: Point;
   loop {
     let x = rand::thread_rng().gen_range(0..19);
     let y = rand::thread_rng().gen_range(0..19);
@@ -14,6 +37,22 @@ pub fn find(color: &Color, point: &Point, board: &Board) -> Point {
       break;
     }
   }
-
   next_point
+}
+
+fn get_point(anchor: &Point, color: &Color, board: &Board) -> u16 {
+  let mut point = 0;
+  let (h_left, h_right) = board.get_directed_points(anchor, Direction::Horizontal);
+  let (v_left, v_right) = board.get_directed_points(anchor, Direction::Vertical);
+  let (i_left, i_right) = board.get_directed_points(anchor, Direction::Increase);
+  let (d_left, d_right) = board.get_directed_points(anchor, Direction::Decrease);
+
+  // println!("{:?} {:?}", h_left, h_right);
+  // println!("{:?} {:?}", v_left, v_right);
+  // println!("{:?} {:?}", i_left, i_right);
+  // println!("{:?} {:?}", d_left, d_right);
+
+  // TODO: pointMap 에서 left/right를 대조해서 점수를 계산한다.
+
+  point
 }
